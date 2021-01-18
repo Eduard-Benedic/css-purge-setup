e8cdfc611555a9ccd954814f17a07cdcde88f5d5
unknown <rafrhodes@uahoo.co.uk>
2021-01-18 14:36:05 +0000

    rename, concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,18 @@
 var postcss = require('gulp-postcss');
 const rename = require('gulp-rename');
+const cleanCSS = require('gulp-clean-css');
+const concat = require('gulp-concat')
 var gulp = require('gulp');
 
 
 gulp.task('css', function () {
      return gulp.src('./src/*.css')
-         .pipe(postcss())
+          .pipe(concat('concat.css'))
+          .pipe(postcss())
+          .pipe(cleanCSS({compatibility: 'ie8'}))
           .pipe(rename({
-              extname: '.csss'
+               extname: '.css',
+               basename: 'campus-purged-ver2'
          }))
          .pipe(gulp.dest('./dest'));
 });
